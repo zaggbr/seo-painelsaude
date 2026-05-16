aaf4cb0d02e02629780833832d14bad27c6a7bef
zaggbr <continentemedia@gmail.com>
2026-05-16 20:51:36 -0300

    fix: sitemap renomeado para sitemap-planos.xml

--- worker.js
+++ worker.js
@@ -2,6 +2,14 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
 
+    if (url.pathname === '/sitemap-planos.xml') {
+      const response = await fetch('https://seo-painelsaude.pages.dev/sitemap-planos.xml');
+      return new Response(response.body, {
+        status: response.status,
+        headers: { 'Content-Type': 'application/xml', 'Cache-Control': 'public, max-age=3600' }
+      });
+    }
+
     if (url.pathname.startsWith('/planos')) {
       const targetURL = `https://seo-painelsaude.pages.dev${url.pathname}${url.search}`;
       const response = await fetch(targetURL, {
